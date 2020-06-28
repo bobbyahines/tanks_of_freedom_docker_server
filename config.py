fddd23bb9620126b20a10b937696ecb93f52e54e
Bobby Hines <bobbyahines@gmail.com>
2020-06-27 22:36:56 -0700

    Python configuration created.

---
+++ config.py
@@ -0,0 +1,11 @@
+
+# database configuration
+MYSQL_HOST = 'db'
+MYSQL_USER = 'user'
+MYSQL_PASSWORD = 'pass'
+MYSQL_DB = 'tof'
+
+
+MAP_CODE_LENGTH = 8
+INTEGRATE_MAP_BROWSER = False
+MAP_IMAGES_FOLDER = "/tmp/"
